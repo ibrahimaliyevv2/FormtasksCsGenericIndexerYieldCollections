be1b749f9dff2fb0fe3414eae51854d7d3274e7a
Ibrahim Aliyev <100702986+ibrahimaliyevv2@users.noreply.github.com>
2022-04-17 00:49:11 +0400

    Update Program.cs

--- Student2Task/Student2Task/Program.cs
+++ Student2Task/Student2Task/Program.cs
@@ -6,7 +6,18 @@ namespace Student2Task
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Student student = new Student();
+
+            student.AddExam("Math", 51);
+            student.AddExam("History", 62);
+            student.AddExam("Biology", 34);
+            student.AddExam("Geography", 45);
+            student.AddExam("Physics", 78);
+            student.AddExam("Chemistry", 92);
+
+            student.GetExamResult("Physics");
+
+            Console.WriteLine(student.GetExamAvg());
         }
     }
 }
