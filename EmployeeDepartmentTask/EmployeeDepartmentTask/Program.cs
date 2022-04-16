8b2ecb71673e34986c7830c4f5eadc6a794f8aa5
Ibrahim Aliyev <100702986+ibrahimaliyevv2@users.noreply.github.com>
2022-04-17 00:23:26 +0400

    Update Program.cs

--- EmployeeDepartmentTask/EmployeeDepartmentTask/Program.cs
+++ EmployeeDepartmentTask/EmployeeDepartmentTask/Program.cs
@@ -6,7 +6,29 @@ namespace EmployeeDepartmentTask
     {
         static void Main(string[] args)
         {
-            
+            Employee employee1 = new Employee();
+            Employee employee2 = new Employee();
+            Employee employee3 = new Employee();
+            Employee employee4 = new Employee();
+            Employee employee5 = new Employee();
+
+            Department department = new Department();
+
+            department.AddEmployee(employee1);
+            department.AddEmployee(employee2);
+            department.AddEmployee(employee3);
+            department.AddEmployee(employee4);
+            department.AddEmployee(employee5);
+
+
+            department.ChangeEmployeeNo(5,4);
+
+            Console.WriteLine("employees ");
+            foreach (var item in department.Employees)
+            {
+                Console.WriteLine(item.No);
+            }
+
         }
     }
 }
